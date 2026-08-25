c6c91c38218ede303c680b5af186a6804f843bd3
JulianTato-0912 <julian.tato@hotmail.com>
2026-08-25 18:22:16 -0300

    Agregar archivos de proyecto.

---
+++ MatesPirru.Backend/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿namespace MatesPirru.Backend.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public int Stock { get; set; }
+        public string UrlImagen { get; set; } = string.Empty;
+        public string Material { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int IdCategoria { get; set; }
+        public bool Activo { get; set; } = true;
+    }
+}
